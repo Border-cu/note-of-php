e57f14b3cd9d09031bc5e80d0ca9a3f08365ce5a
Border <51527870+Border-cu@users.noreply.github.com>
2023-11-08 23:08:30 +0800

    Add files via upload

---
+++ 3复选框2/多选框.php
@@ -0,0 +1,23 @@
+<?php
+
+    //将复选框内容存储到数据库
+
+    /*表示设计
+     *
+     *      id      name    gender      hobby
+     *      1       mark    男          篮球，足球，乒乓球
+    */
+    
+    header('Content-type;text/html;charset=utf-8');
+    $hobby = isset($_POST['hobby']) ? $_POST['hobby'] : array();
+    //print_r($hobby);
+
+    //数组转化成有格式的字符串
+    $hobby_string = implode('-',$hobby);
+    echo $hobby_string;
+
+    //假设$hobby_string是从数据库取出来的
+    //炸成数组
+    var_dump(explode('|',$hobby_string));
+
+?>
